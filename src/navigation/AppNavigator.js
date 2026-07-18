83969c88a97f114a46132094150c17ec3a81c930
anulikamadueagles <anulikamadueagles@gmail.com>
2026-07-18 08:49:27 +0100

    Added voice and video call navigation

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,46 +1,16 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import ChatsScreen from "../screens/ChatsScreen";
-import ChatScreen from "../screens/ChatScreen";
-import ContactsScreen from "../screens/ContactsScreen";
 import PrivateChatScreen from "../screens/PrivateChatScreen";
+import VoiceCallScreen from "../screens/VoiceCallScreen";
+import VideoCallScreen from "../screens/VideoCallScreen";
 import StatusScreen from "../screens/StatusScreen";
 import CallsScreen from "../screens/CallsScreen";
 import SettingsScreen from "../screens/SettingsScreen";
-import SearchScreen from "../screens/SearchScreen";
-import GroupsScreen from "../screens/GroupsScreen";
-import GroupChatScreen from "../screens/GroupChatScreen";
-import ProfileScreen from "../screens/ProfileScreen";
-import VoiceCallScreen from "../screens/VoiceCallScreen";
-import VideoCallScreen from "../screens/VideoCallScreen";
-import IncomingCallScreen from "../screens/IncomingCallScreen";
 
 const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
-
-function HomeTabs() {
-
-  return (
-
-    <Tab.Navigator>
-
-      <Tab.Screen name="Chats" component={ChatsScreen} />
-      <Tab.Screen name="Contacts" component={ContactsScreen} />
-      <Tab.Screen name="Search" component={SearchScreen} />
-      <Tab.Screen name="Groups" component={GroupsScreen} />
-      <Tab.Screen name="Status" component={StatusScreen} />
-      <Tab.Screen name="Calls" component={CallsScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
-
-    </Tab.Navigator>
-
-  );
-
-}
 
 export default function AppNavigator() {
 
@@ -48,27 +18,21 @@ export default function AppNavigator() {
 
     <NavigationContainer>
 
-      <Stack.Navigator>
+      <Stack.Navigator
+        initialRouteName="Chats"
+        screenOptions={{
+          headerShown:false
+        }}
+      >
 
         <Stack.Screen
-          name="Home"
-          component={HomeTabs}
-          options={{ headerShown:false }}
-        />
-
-        <Stack.Screen
-          name="Chat"
-          component={ChatScreen}
+          name="Chats"
+          component={ChatsScreen}
         />
 
         <Stack.Screen
           name="PrivateChat"
           component={PrivateChatScreen}
-        />
-
-        <Stack.Screen
-          name="GroupChat"
-          component={GroupChatScreen}
         />
 
         <Stack.Screen
@@ -82,8 +46,18 @@ export default function AppNavigator() {
         />
 
         <Stack.Screen
-          name="IncomingCall"
-          component={IncomingCallScreen}
+          name="Status"
+          component={StatusScreen}
+        />
+
+        <Stack.Screen
+          name="Calls"
+          component={CallsScreen}
+        />
+
+        <Stack.Screen
+          name="Settings"
+          component={SettingsScreen}
         />
 
       </Stack.Navigator>
